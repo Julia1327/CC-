6fa35018560b1258d4a12f51fdd80143d997e3c7
«Julia» <juliya454@yandex.ru>
2023-03-29 23:17:59 +0300

    Написали приветствие

--- example_IfElse/Program.cs
+++ example_IfElse/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "юля")
+{
+    Console.WriteLine("Ура, это же ЮЛЯ!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
